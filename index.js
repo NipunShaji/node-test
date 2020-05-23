f54c3124a20c5652a36f6364a7f76109ad708ef3
nipunshaji <nipunshaji9447@gmail.com>
2020-05-23 09:23:09 +0530

    kkjdcs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http');
+const url = require('url');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url,true).query;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end('<h1>Hello World</h1><p>querry object : '+querryObject+'</p>');
 });
 
 server.listen(port,() => {
