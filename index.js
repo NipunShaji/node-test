212c2b25f767fca05d987f231c7d313411eb8263
nipunshaji <nipunshaji9447@gmail.com>
2020-05-23 09:48:47 +0530

    kkjdcs

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
   var code = ''
+  var type = ''
   try {
     code = req.param('code');
   } catch (e) {
     code = 'no code'
   }
+  try {
+    type = req.param('type');
+  } catch (e) {
+    type = 'no type'
+  }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1><p>querry object : '+code+'</p>');
+  res.end('<h1>Hello World</h1><p>querry object : '+code+'</p><p>type : '+type+'</p>');
 })
 
 var server = app.listen(port,() => {
